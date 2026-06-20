7b032640f8c53cfc85c973473a2fbfb030ada2cb
Natalino Gomes <intgomes657@gmail.com>
2026-06-20 13:43:17 +0100

    start to implemente postgres whit prisma

---
+++ jobtracker/prisma/migrations/20260620120819_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Application" (
+    "id" TEXT NOT NULL,
+    "company" TEXT NOT NULL,
+    "role" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "note" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Application_pkey" PRIMARY KEY ("id")
+);
