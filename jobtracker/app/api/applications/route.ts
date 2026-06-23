fcc070819b6b164b8fecb76679d32f976cc6d812
Natalino Gomes <intgomes657@gmail.com>
2026-06-23 12:27:40 +0100

    migration to prisma

--- jobtracker/app/api/applications/route.ts
+++ jobtracker/app/api/applications/route.ts
@@ -1,20 +1,24 @@
 import { applications } from "@/lib/applications-store";
 
+import { prisma } from "@/lib/prisma";
+
 export async function GET() {
+  const applications = await prisma.application.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
   return Response.json(applications);
 }
 
 export async function POST(request: Request) {
   const body = await request.json();
 
-  console.log("inside POST log", body);
+  //console.log("inside POST log", body);
 
-  const newApplication = {
-    id: crypto.randomUUID(),
-    ...body,
-  };
-
-  applications.push(newApplication);
+  const newApplication = await prisma.application.create({
+    data: body,
+  });
 
   return Response.json(newApplication, {
     status: 201,
